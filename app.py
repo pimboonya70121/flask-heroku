488d839b9590665f72ddecba7c8840e783dcf84e
pimboonya70121 <90390491+pimboonya70121@users.noreply.github.com>
2021-09-09 19:21:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,16 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok" 
+    return "Hello พิมบุญญา ล้อมวงษ์ เลขที่23 ม.4/4" 
 
 @app.route('/api', methods=['GET'])
 def get_api():
     return jsonify(data)
+
+@app.route('/hi')
+@app.route('/hello')
+def hi():
+    return "สวัสดี"
 
 if __name__ == "__main__":
     app.run(debug=False)
